f0fd99327e11b2dd262370899d0311515f46163f
Daniel Gandia <gandiadaniel@gmail.com>
2021-04-03 11:17:57 -0400

    Updating the schema file

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE departments (
 );
 -- Adding new table
 CREATE TABLE employees (
-	 emp_no INT NOT NULL,
+     emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
@@ -33,20 +33,19 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 CREATE TABLE titles (
-	emp_no INT NOT NULL,
-    title INT NOT NULL,
-	from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
+  emp_no INT NOT NULL,
+  title INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-    PRIMARY KEY (emp_no)
+    PRIMARY KEY (emp_no, from_date)
 );
 CREATE TABLE dept_employee (
-	dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
+   emp_no INT NOT NULL,
+   dept_no VARCHAR(4) NOT NULL,
+   from_date DATE NOT NULL,
+   to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
-SELECT * FROM dept_employee;
